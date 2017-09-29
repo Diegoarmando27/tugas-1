e58c6ae5b6238a60879a2b1c8a497bb77f7ff0dd
Diegoarmando27 <32378342+Diegoarmando27@users.noreply.github.com>
2017-09-29 10:23:24 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,7 @@
+nama="DIEGO ARMANDO PRATAMA PUTRA"
+nim="043160028"
+umur=19
+print("nama:"+nama)
+print("nim:"+nim)
+print("umurku sekarang:",str(umur),"tahun")
+print("umurku 5 tahun kedepan:",str(umur+5),"tahun")  
